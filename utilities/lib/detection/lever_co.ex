0ffc6acdf9eaf90f0bbfb4436955c1fd6bec5007
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2022-06-06 19:18:46 -0400

    Finalize first draft of detection and transformation for all 3 types
    Commented out working through Greenhouse's nested list type that seemed to be in a span element.
    Covering the URL for Lever.co at https://jobs.lever.co/modsquad/b2e49c2a-4eac-4fdb-94b6-ef8523cd124c and Greenhouse.io at https://boards.greenhouse.io/fireworkcareers/jobs/4407232004#app.
    Strict markdown seems to work well for Lever as there were a lot of styling spans that didn't translate otherwise but for some reason Greenhouse leaves similar spans in.
    
    I don't know what kind of solution I'm looking for here honestly. 100% perfect? I may need to literally adjust every single job because Firework may be different than another company's portal. TDD might suss out some of these issues but I have more fun with the "Livebook Driven Development" I'm doing now. Every type may also warrant an "escape hatch" sooner rather than later as easy job may require custom yak shaving.

--- utilities/lib/detection/lever_co.ex
+++ utilities/lib/detection/lever_co.ex
@@ -5,6 +5,40 @@ defmodule Utilities.Detection.LeverCo do
 
   def find_description(document) do
     document
-    |> Floki.find(".content-wrapper posting-page")
+    |> Floki.find(".content-wrapper.posting-page")
+  end
+
+  def transform_description(document) do
+    description =
+      find_description(document)
+      |> hd()
+      |> Floki.children(include_text: false)
+      |> Floki.traverse_and_update(fn
+        {"li", attrs, [text]} -> {"li", attrs, ["[ ] " <> text]}
+        tag -> tag
+      end)
+      |> Floki.raw_html()
+
+    {:ok, markdown} = Pandex.html_to_markdown_strict(description)
+
+    markdown_title = transform_title(document)
+
+    # Cleanup escaped brackets [ ]
+    content = markdown |> String.replace("\\[ \\]", "[ ]") |> String.replace("-   [ ]", "* [ ]")
+
+    # Inject our title as the first line
+    markdown_content = "# #{markdown_title}\n\n" <> content
+
+    [title: markdown_title, content: markdown_content]
+  end
+
+  def transform_title(document) do
+    title =
+      Floki.find(document, "title")
+      |> hd()
+      |> Floki.raw_html()
+
+    # Since this is a single tag we can just convert it to Markdown directly
+    title |> String.replace("<title>", "") |> String.replace("</title>", "")
   end
 end
